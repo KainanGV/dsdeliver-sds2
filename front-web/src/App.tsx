e34bee54bbee740dec2d326ae506c2d673252209
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-01-07 22:45:44 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css';
 import NavBar from './NavBar';
-import Home from './Home';
+import Routes from './Routes';
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <Home />
-      
-    </>
+      <Routes />
   );
 }
 
