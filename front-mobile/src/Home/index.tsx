f3842a01bdac52bc406450d8bb66b319f0d849f6
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-01-11 23:41:47 -0300

    Add Header

---
+++ front-mobile/src/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+function Home(){
+    return (
+        <View>
+            {/*<Image source={require('../assets/logo.png')}/>*/}
+            <Text >Conteúdo da página Home</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Home;
